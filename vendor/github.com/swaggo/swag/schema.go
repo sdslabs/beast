402fc56281287e5db51a60814e923241b5da4937
Deepesh Pathak <deepshpathak@gmail.com>
2018-10-05 05:46:59 +0530

    docs: add swagger for api docs with gin

---
+++ vendor/github.com/swaggo/swag/schema.go
@@ -0,0 +1,33 @@
+package swag
+
+import "fmt"
+
+// CheckSchemaType TODO: NEEDS COMMENT INFO
+func CheckSchemaType(typeName string) {
+	switch typeName {
+	case "string", "number", "integer", "boolean", "array", "object":
+
+	default:
+		panic(fmt.Errorf("%s is not basic types", typeName))
+	}
+}
+
+// TransToValidSchemeType indicates type will transfer golang basic type to swagger supported type.
+func TransToValidSchemeType(typeName string) string {
+	switch typeName {
+	case "uint", "int", "uint8", "int8", "uint16", "int16", "byte":
+		return "integer"
+	case "uint32", "int32", "rune":
+		return "integer"
+	case "uint64", "int64":
+		return "integer"
+	case "float32", "float64":
+		return "number"
+	case "bool":
+		return "boolean"
+	case "string":
+		return "string"
+	default:
+		return typeName // to support user defined types
+	}
+}
