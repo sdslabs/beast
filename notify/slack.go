9c8c9d4710205456ee11053cb8a27fb635fc30fd
Deepesh Pathak <deepshpathak@gmail.com>
2018-12-26 00:46:41 +0530

    notify: add feature to send notification to slack.

--- notify/slack.go
+++ notify/slack.go
@@ -1,30 +1,33 @@
 package notify
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/sdslabs/beastv4/core/config"
+	log "github.com/sirupsen/logrus"
 )
 
 type Attachment struct {
-	Fallback     *string   `json:"fallback"`
-	Color        *string   `json:"color"`
-	PreText      *string   `json:"pretext"`
-	AuthorName   *string   `json:"author_name"`
-	AuthorLink   *string   `json:"author_link"`
-	AuthorIcon   *string   `json:"author_icon"`
-	Title        *string   `json:"title"`
-	TitleLink    *string   `json:"title_link"`
-	Text         *string   `json:"text"`
-	ImageUrl     *string   `json:"image_url"`
-	Fields       []*Field  `json:"fields"`
-	Footer       *string   `json:"footer"`
-	FooterIcon   *string   `json:"footer_icon"`
-	Timestamp    *int64    `json:"ts"`
-	MarkdownIn   *[]string `json:"mrkdwn_in"`
-	Actions      []*Action `json:"actions"`
-	CallbackID   *string   `json:"callback_id"`
-	ThumbnailUrl *string   `json:"thumb_url"`
+	Fallback     string            `json:"fallback,omitempty"`
+	Color        NotificationColor `json:"color,omitempty"`
+	PreText      string            `json:"pretext,omitempty"`
+	AuthorName   string            `json:"author_name,omitempty"`
+	AuthorLink   string            `json:"author_link,omitempty"`
+	AuthorIcon   string            `json:"author_icon,omitempty"`
+	Title        string            `json:"title,omitempty"`
+	TitleLink    string            `json:"title_link,omitempty"`
+	Text         string            `json:"text,omitempty"`
+	ImageUrl     string            `json:"image_url,omitempty"`
+	Footer       string            `json:"footer,omitempty"`
+	FooterIcon   string            `json:"footer_icon,omitempty"`
+	Timestamp    int64             `json:"ts,omitempty"`
+	MarkdownIn   []string          `json:"mrkdwn_in,omitempty"`
+	CallbackID   string            `json:"callback_id,omitempty"`
+	ThumbnailUrl string            `json:"thumb_url,omitempty"`
 }
 
 type SlackPostPayload struct {
@@ -63,7 +66,8 @@ func (notifier *SlackNotifier) SendNotification() error {
 		return fmt.Errorf("Error while converting payload to JSON : %s", err)
 	}
 
-	req, err := http.NewRequest("POST", notifier.WebHookURL, payload)
+	payloadReader := bytes.NewReader(payload)
+	req, err := http.NewRequest("POST", notifier.WebHookURL, payloadReader)
 	if err != nil {
 		return fmt.Errorf("Error while connecting to webhook url host : %s", err)
 	}
@@ -73,6 +77,52 @@ func (notifier *SlackNotifier) SendNotification() error {
 	_, err = client.Do(req)
 
 	if err != nil {
-		fmt.Errorf("Error while posting payload for notification : %s", err)
+		return fmt.Errorf("Error while posting payload for notification : %s", err)
 	}
+
+	return nil
+}
+
+func SendNotificationToSlack(nType NotificationType, msg string) error {
+	if config.Cfg.SlackWebHookURL == "" {
+		log.Warnf("No slack webhook url provided in beast config, cannot send notification.")
+		return fmt.Errorf("No webhook URL in beast config.")
+	}
+
+	slackNotifier := NewSlackNotifier(config.Cfg.SlackWebHookURL)
+	slackNotifier.SlackPostPayload = SlackPostPayload{
+		Username: "Beast",
+		IconUrl:  "https://i.ibb.co/sjC5dRY/beast-eye-39371.png",
+		Channel:  "#beast",
+	}
+
+	nAttachment := Attachment{
+		AuthorName: "Beast Notifier",
+		AuthorLink: "https://backdoor.sdslabs.co",
+		Footer:     "Beast Slack API",
+		FooterIcon: "https://platform.slack-edge.com/img/default_application_icon.png",
+		Timestamp:  time.Now().Unix(),
+		Text:       msg,
+	}
+
+	switch nType {
+	case Success:
+		nAttachment.Color = SuccessColor
+		nAttachment.Title = "Beast Deployment Success"
+		break
+	case Error:
+		nAttachment.Color = ErrorColor
+		nAttachment.Title = "Beast Deployment Error"
+		break
+	}
+
+	slackNotifier.SlackPostPayload.Attachments = []Attachment{nAttachment}
+	err := slackNotifier.SendNotification()
+	if err != nil {
+		log.Errorf("Error while sending notification to slack : %s", err)
+		return fmt.Errorf("NOTIFICATION_SEND_ERROR: %s", err)
+	}
+
+	log.Infof("Notfication sent to slack.")
+	return nil
 }
