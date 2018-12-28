95f85dca5a9811f811d5c8ad870462d38b350049
Deepesh Pathak <deepshpathak@gmail.com>
2018-12-29 02:54:27 +0530

    templates: add xinetd challenge config.

---
+++ templates/xinetd.go
@@ -0,0 +1,24 @@
+package templates
+
+var XINETD_CONFIGURATION_TEMPLATE string = `\
+service {{.ServiceName}} 
+{
+    disable 	= no
+    type        = UNLISTED
+    wait        = no
+    server      = /bin/sh
+    server_args = -c cd${IFS}/challenge;exec${IFS}{{.ServicePath}}
+    socket_type = stream
+    protocol    = tcp
+    user        = beast 
+    port        = {{.Port}}
+    bind        = 0.0.0.0
+    banner_fail = /etc/banner_fail
+
+    instances   = UNLIMITED
+    flags       = REUSE
+    per_source	= 5     # Maximum instances of this service per source IP address
+    rlimit_cpu	= 20    # Maximum number of CPU seconds that the service may use.
+    rlimit_as   = 512M  # Address Space resource limit for the service
+}
+`
