9d9260374372bb9afe13a262f0d8f4ecf0247dee
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-03-29 20:01:34 +0530

    function added in transaction.go

--- database/transactions.go
+++ database/transactions.go
@@ -1,12 +1,33 @@
 package database
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
 )
 
 type Transaction struct {
 	gorm.Model
-	Action string
-	User   string
+	Action      string
+	UserId        string
+	ChallengeId uint
+}
+
+func SaveTransaction(transaction *Transaction) error {
+
+	DBMux.Lock()
+	defer DBMux.Unlock()
+
+	tx := Db.Begin()
+
+	if tx.Error != nil {
+		return fmt.Errorf("Error while saving transaction", tx.Error)
+	}
+
+	if err := tx.FirstOrCreate(transaction, *transaction).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+	return tx.Commit().Error
 }
