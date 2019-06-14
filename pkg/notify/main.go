9c81c47c1450e35d7d840216684c6da7e3fcd80f
shubhamgupta2956 <shubham.gupta2956@gmail.com>
2019-06-14 10:11:33 +0530

    tried to improve notification structure

---
+++ pkg/notify/main.go
@@ -0,0 +1,74 @@
+package notify
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Attachment struct {
+	Fallback     string            `json:"fallback,omitempty"`
+	Color        NotificationColor `json:"color,omitempty"`
+	PreText      string            `json:"pretext,omitempty"`
+	AuthorName   string            `json:"author_name,omitempty"`
+	AuthorLink   string            `json:"author_link,omitempty"`
+	AuthorIcon   string            `json:"author_icon,omitempty"`
+	Title        string            `json:"title,omitempty"`
+	TitleLink    string            `json:"title_link,omitempty"`
+	Text         string            `json:"text,omitempty"`
+	ImageUrl     string            `json:"image_url,omitempty"`
+	Footer       string            `json:"footer,omitempty"`
+	FooterIcon   string            `json:"footer_icon,omitempty"`
+	Timestamp    int64             `json:"ts,omitempty"`
+	MarkdownIn   []string          `json:"mrkdwn_in,omitempty"`
+	CallbackID   string            `json:"callback_id,omitempty"`
+	ThumbnailUrl string            `json:"thumb_url,omitempty"`
+}
+
+type PostPayload struct {
+	Parse       string       `json:"parse,omitempty"`
+	Username    string       `json:"username,omitempty"`
+	IconUrl     string       `json:"icon_url,omitempty"`
+	IconEmoji   string       `json:"icon_emoji,omitempty"`
+	Channel     string       `json:"channel,omitempty"`
+	Text        string       `json:"text,omitempty"`
+	Attachments []Attachment `json:"attachments,omitempty"`
+	Markdown    bool         `json:"mrkdwn,omitempty"`
+}
+
+type Notifier struct {
+	WebHookURL  string
+	PostPayload PostPayload
+}
+
+func (notifier *Notifier) SendNotification() error {
+	if notifier.WebHookURL == "" {
+		return fmt.Errorf("Need a WebHookURL to send notification.")
+	}
+
+	if notifier.PostPayload.Channel == "" || notifier.PostPayload.Username == "" {
+		return fmt.Errorf("Username and Channel required to send the notification.")
+	}
+
+	payload, err := json.Marshal(notifier.PostPayload)
+	if err != nil {
+		return fmt.Errorf("Error while converting payload to JSON : %s", err)
+	}
+
+	payloadReader := bytes.NewReader(payload)
+	req, err := http.NewRequest("POST", notifier.WebHookURL, payloadReader)
+	if err != nil {
+		return fmt.Errorf("Error while connecting to webhook url host : %s", err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	client := http.Client{}
+	_, err = client.Do(req)
+
+	if err != nil {
+		return fmt.Errorf("Error while posting payload for notification : %s", err)
+	}
+
+	return nil
+}
--- pkg/notify/slack.go
+++ pkg/notify/slack.go
@@ -1,86 +1,17 @@
 package notify
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"time"
 
 	"github.com/sdslabs/beastv4/core/config"
 	log "github.com/sirupsen/logrus"
 )
 
-type Attachment struct {
-	Fallback     string            `json:"fallback,omitempty"`
-	Color        NotificationColor `json:"color,omitempty"`
-	PreText      string            `json:"pretext,omitempty"`
-	AuthorName   string            `json:"author_name,omitempty"`
-	AuthorLink   string            `json:"author_link,omitempty"`
-	AuthorIcon   string            `json:"author_icon,omitempty"`
-	Title        string            `json:"title,omitempty"`
-	TitleLink    string            `json:"title_link,omitempty"`
-	Text         string            `json:"text,omitempty"`
-	ImageUrl     string            `json:"image_url,omitempty"`
-	Footer       string            `json:"footer,omitempty"`
-	FooterIcon   string            `json:"footer_icon,omitempty"`
-	Timestamp    int64             `json:"ts,omitempty"`
-	MarkdownIn   []string          `json:"mrkdwn_in,omitempty"`
-	CallbackID   string            `json:"callback_id,omitempty"`
-	ThumbnailUrl string            `json:"thumb_url,omitempty"`
-}
-
-type SlackPostPayload struct {
-	Parse       string       `json:"parse,omitempty"`
-	Username    string       `json:"username,omitempty"`
-	IconUrl     string       `json:"icon_url,omitempty"`
-	IconEmoji   string       `json:"icon_emoji,omitempty"`
-	Channel     string       `json:"channel,omitempty"`
-	Text        string       `json:"text,omitempty"`
-	Attachments []Attachment `json:"attachments,omitempty"`
-	Markdown    bool         `json:"mrkdwn,omitempty"`
-}
-
-type SlackNotifier struct {
-	WebHookURL       string
-	SlackPostPayload SlackPostPayload
-}
-
-func NewSlackNotifier(webhookUrl string) *SlackNotifier {
-	return &SlackNotifier{
+func NewSlackNotifier(webhookUrl string) *Notifier {
+	return &Notifier{
 		WebHookURL: webhookUrl,
 	}
-}
-
-func (notifier *SlackNotifier) SendNotification() error {
-	if notifier.WebHookURL == "" {
-		return fmt.Errorf("Need a WebHookURL to send notification.")
-	}
-
-	if notifier.SlackPostPayload.Channel == "" || notifier.SlackPostPayload.Username == "" {
-		return fmt.Errorf("Username and Channel required to send the notification.")
-	}
-
-	payload, err := json.Marshal(notifier.SlackPostPayload)
-	if err != nil {
-		return fmt.Errorf("Error while converting payload to JSON : %s", err)
-	}
-
-	payloadReader := bytes.NewReader(payload)
-	req, err := http.NewRequest("POST", notifier.WebHookURL, payloadReader)
-	if err != nil {
-		return fmt.Errorf("Error while connecting to webhook url host : %s", err)
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	client := http.Client{}
-	_, err = client.Do(req)
-
-	if err != nil {
-		return fmt.Errorf("Error while posting payload for notification : %s", err)
-	}
-
-	return nil
 }
 
 func SendNotificationToSlack(nType NotificationType, msg string) error {
@@ -90,7 +21,7 @@ func SendNotificationToSlack(nType NotificationType, msg string) error {
 	}
 
 	slackNotifier := NewSlackNotifier(config.Cfg.SlackWebHookURL)
-	slackNotifier.SlackPostPayload = SlackPostPayload{
+	slackNotifier.PostPayload = PostPayload{
 		Username: "Beast",
 		IconUrl:  "https://i.ibb.co/sjC5dRY/beast-eye-39371.png",
 		Channel:  "#beast",
@@ -116,7 +47,7 @@ func SendNotificationToSlack(nType NotificationType, msg string) error {
 		break
 	}
 
-	slackNotifier.SlackPostPayload.Attachments = []Attachment{nAttachment}
+	slackNotifier.PostPayload.Attachments = []Attachment{nAttachment}
 	err := slackNotifier.SendNotification()
 	if err != nil {
 		log.Errorf("Error while sending notification to slack : %s", err)
