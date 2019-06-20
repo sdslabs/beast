361dafd5dab70f601ffb31c73718be74666026e9
Piyush Sethia <piyushsethia1999@gmail.com>
2019-06-21 01:04:25 +0530

    Improves filename

--- scripts/test/test_examples.sh
+++ scripts/test/test_examples.sh
@@ -7,34 +7,38 @@ SUCCESS="\e[32;1mSUCCESS\e[0m"
 INFO="\e[34;1mINFO\e[0m"
 
 checkPortAvailable() {
-	local port=$1
-	echo -e "$INFO : $CHALLENGE : Check port availability"
+	local challenge=$1
+	local port=$2
+	echo -e "$INFO : $challenge : Check port availability"
 	(echo -e 1 > /dev/tcp/127.0.0.1/$port) 2> /dev/null
 	if [[ $? -eq 0 ]]; then
-		echo -e "$ERROR : $CHALLENGE : port $port is not free, cannot test challenge"
+		echo -e "$ERROR : $challenge : port $port is not free, cannot test challenge"
 		exit 1
 	fi
 }
 
 deployChallenge() {
-	local challdir=$1
-	echo -e "$INFO : $CHALLENGE : Start deploy"
+	local challenge=$1
+	local challdir=$2
+	echo -e "$INFO : $challenge : Start deploy"
 	beast -v challenge deploy --local-directory $challdir
 	if [[ $? -ne 0 ]]; then
-		echo -e "$ERROR: $CHALLENGE : There was an error in deployment of challenge"
+		echo -e "$ERROR: $challenge : There was an error in deployment of challenge"
 		exit 1
 	fi
 }
 
 checkPortReachable() {
-	(echo -e 1 > /dev/tcp/127.0.0.1/$PORT) 2> /dev/null
+	local port=$1
+	(echo -e 1 > /dev/tcp/127.0.0.1/$port) 2> /dev/null
 }
 
 purge() {
-	echo -e "$INFO : $CHALLENGE : Purge challenge"
-	beast -v challenge purge $CHALLENGE -d
+	local challenge=$1
+	echo -e "$INFO : $challenge : Purge challenge"
+	beast -v challenge purge $challenge -d
 	if [[ $? -ne 0 ]]; then
-		echo -e "$ERROR : $CHALLENGE : Error while purging"
+		echo -e "$ERROR : $challenge : Error while purging"
 		exit 1	
 	fi
 }
@@ -48,20 +52,18 @@ doHTTPProbe() {
 CHALLENGE="simple"
 PORT=10001
 ## Check if port is taken
-checkPortAvailable $PORT
+checkPortAvailable $CHALLENGE $PORT
 ## Deploy challenge
-deployChallenge $PWD/_examples/$CHALLENGE
+deployChallenge $CHALLENGE $PWD/_examples/$CHALLENGE
 ## Test deployment
 echo -e "$INFO : $CHALLENGE : Test deployment"
-checkPortReachable
+checkPortReachable $PORT
 if [[ $? -eq 0 ]]; then
-	echo -e "$SUCCESS: $CHALLENGE: Deployed successfully"
+	echo -e "$SUCCESS: $CHALLENGE : Deployed successfully"
 else
 	echo -e "$ERROR: $CHALLENGE : There was an error in deployment of challenge"
 	exit 1	
 fi
-## Purge
-purge
 
 #Test challenge static-chall
 CHALLENGE="static-chall"
@@ -73,7 +75,7 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 ## Deploy challenge
-deployChallenge $PWD/_examples/$CHALLENGE
+deployChallenge $CHALLENGE $PWD/_examples/$CHALLENGE
 ## Test deployment
 echo -e "$INFO : $CHALLENGE : Test deployment"
 response_code=$(doHTTPProbe "http://static.beast.sdslabs.co/static/$CHALLENGE/index.html")
@@ -83,16 +85,14 @@ else
 	echo -e "$ERROR : $CHALLENGE : There was an error in deployment of challenge"
 	exit 1
 fi
-## Purge
-purge
 
 # Test challenge web-php
 CHALLENGE="web-php"
 PORT=10002
 ## Check if port is taken
-checkPortAvailable $PORT
+checkPortAvailable $CHALLENGE $PORT
 ## Deploy challenge
-deployChallenge $PWD/_examples/$CHALLENGE
+deployChallenge $CHALLENGE $PWD/_examples/$CHALLENGE
 ## Test deployment
 echo -e "$INFO : $CHALLENGE : Test deployment"
 response_code=$(doHTTPProbe "http://localhost:$PORT/index.php")
@@ -102,14 +102,12 @@ else
 	echo -e "$ERROR : $CHALLENGE : There was an error in deployment of challenge"
 	exit 1	
 fi
-## Purge
-purge
 
 # Test challenge web-php-mysql
 CHALLENGE="web-php-mysql"
 PORT=10004
 ## Check if port is taken
-checkPortAvailable $PORT
+checkPortAvailable $CHALLENGE $PORT
 ## Test beast-mysql container
 echo -e "$INFO : $CHALLENGE : Test beast-mysql container"
 docker ps | grep -q 'beast-mysql'
@@ -118,7 +116,7 @@ if [[ $? -ne 0 ]]; then
 	exit 1
 fi
 ## Deploy challenge
-deployChallenge $PWD/_examples/$CHALLENGE
+deployChallenge $CHALLENGE $PWD/_examples/$CHALLENGE
 ## Test deployment
 echo -e "$INFO : $CHALLENGE : Test deployment"
 response_code=$(doHTTPProbe "http://localhost:$PORT/index.php")
@@ -128,24 +126,32 @@ else
 	echo -e "$ERROR : $CHALLENGE : There was an error in deployment of challenge"
 	exit 1	
 fi
-## Purge
-purge
 
 # Test challenge xinetd-service
 CHALLENGE="xinetd-service"
 PORT=10003
 ## Check if port is taken
-checkPortAvailable $PORT
+checkPortAvailable $CHALLENGE $PORT
 ## Deploy challenge
-deployChallenge $PWD/_examples/$CHALLENGE
+deployChallenge $CHALLENGE $PWD/_examples/$CHALLENGE
 ## Test deployment
 echo -e "$INFO : $CHALLENGE : Test deployment"
-checkPortReachable
+checkPortReachable $PORT
 if [[ $? -eq 0 ]]; then
 	echo -e "$SUCCESS : $CHALLENGE : Deployed successfully"
 else
 	echo -e "$ERROR : $CHALLENGE : There was an error in deployment of challenge"
 	exit 1	
 fi
-## Purge
-purge
+
+## Purge all challenges
+# simple
+purge simple
+# static-chall
+purge static-chall
+# web-php
+purge web-php
+# web-php-mysql
+purge web-php-mysql
+# xinetd-service
+purge xinetd-service
