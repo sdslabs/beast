c6e24c13310ce72cd9a08219f0d2049a1a38d7f9
Aryan <krish7664@gmail.com>
2025-02-13 08:51:25 +0530

    Adds route for send otp for forgot password

--- api/router.go
+++ api/router.go
@@ -36,6 +36,7 @@ func initGinRouter() *gin.Engine {
 		authGroup.POST("/login", login)
 		authGroup.POST("/reset-password", authorize, resetPasswordHandler)
 		authGroup.POST("/send-otp", sendOTPHandler)
+		authGroup.POST("/send-otp-forget", sendOTPForForgetHandler)
 		authGroup.POST("/verify-otp", verifyOTPHandler)
 		authGroup.POST("/verify-otp-forget", verifyOTPForForgetHandler)
 	}
