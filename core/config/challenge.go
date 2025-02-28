740719ae9238f12f6eb4de1c64530064b7955c81
sukhman <sukhmansinghsaluja@gmail.com>
2025-03-01 01:33:27 +0530

    Revert "Add service_docker type chall"
    This reverts commit c8a2b47fb5bd9cccb322ad06233c63a9b6d2daad.

--- core/config/challenge.go
+++ core/config/challenge.go
@@ -204,13 +204,16 @@ func (config *ChallengeMetadata) ValidateRequiredFields() (error, bool) {
 // # Dependencies required by challenge, installed using default package manager of base image apt for most cases.
 // apt_deps = ["", ""]
 //
+//
 // # A list of setup scripts to run for building challenge enviroment.
 // # Keep in mind that these are only for building the challenge environment and are executed
 // # in the iamge building step of the deployment pipeline.
 // setup_scripts = ["", ""]
 //
+//
 // # A directory containing any of the static assets for the challenge, exposed by beast static endpoint.
 // static_dir = ""
+//
 //
 // # Command to execute inside the container, if a predefined type is being used try to
 // # use an existing field to let beast automatically calculate what command to run.
@@ -218,42 +221,42 @@ func (config *ChallengeMetadata) ValidateRequiredFields() (error, bool) {
 // # of the service using service_path field.
 // run_cmd = ""
 //
+//
 // # Similar to run_cmd but in this case you have the entire container to yourself
 // # and everything you are doing is done using root permissions inside the container
 // # When using this keep in mind you are root inside the container.
 // entrypoint = ""
+//
 //
 // # Relative path to binary which needs to be executed when the specified
 // # Type for the challenge is service.
 // # This can be anything which can be exeucted, a python file, a binary etc.
 // service_path = ""
 //
+//
 // # Relative directory corresponding to root of the challenge where the root
 // # of the web application lies.
 // web_root = ""
+//
 //
 // # Any custom base image you might want to use for your particular challenge.
 // # Exists for flexibility reasons try to use existing base iamges wherever possible.
 // base_image = ""
 //
+//
 // # Docker file name for specific type challenge - `docker`.
 // # Helps to build flexible images for specific user-custom challenges
 // docket_context = ""
 //
-// # Xinetd.conf file name for specific type challenge - `service_docker`.
-// # Helps to build flexible images for specific user-custom challenges
-// xinetd_conf = ""
 //
 // # Environment variables that can be used in the application code.
 // [[var]]
-//
-//	key = ""
-//	value = ""
+//     key = ""
+//     value = ""
 //
 // [[var]]
-//
-//	key = ""
-//	value = ""
+//     key = ""
+//     value = ""
 //
 // Type of traffic to expose through the port mapping provided.
 // traffic = "udp" / "tcp"
@@ -271,7 +274,6 @@ type ChallengeEnv struct {
 	ServicePath      string           `toml:"service_path"`
 	Entrypoint       string           `toml:"entrypoint"`
 	DockerCtx        string           `toml:"docker_context"`
-	XinetdConf       string           `toml:"xinetd_conf"`
 	EnvironmentVars  []EnvironmentVar `toml:"var"`
 	Traffic          string           `toml:"traffic"`
 }
@@ -496,27 +498,11 @@ func (config *ChallengeEnv) ValidateRequiredFields(challType string, challdir st
 
 	if challType == core.DOCKER_CHALLENGE_TYPE_NAME {
 		if config.DockerCtx == "" {
-			return errors.New("docker Context file not provided in docker-type challenge")
+			return errors.New("Docker Context file not provided in docker-type challenge")
 		} else if filepath.IsAbs(config.DockerCtx) {
-			return fmt.Errorf("for challenge type `docker-type` docker_context is a required variable, which should be relative path to docker context file.")
+			return fmt.Errorf("For challenge type `docker-type` docker_context is a required variable, which should be relative path to docker context file.")
 		} else if err := utils.ValidateFileExists(filepath.Join(challdir, config.DockerCtx)); err != nil {
-			return fmt.Errorf("file : %s does not exist", config.DockerCtx)
-		}
-	} else if challType == core.SERVICE_DOCKER_CHALLENGE_TYPE_NAME {
-		if config.DockerCtx == "" {
-			return errors.New("docker Context file not provided in docker-type challenge")
-		} else if filepath.IsAbs(config.DockerCtx) {
-			return fmt.Errorf("for challenge type `docker-type` docker_context is a required variable, which should be relative path to docker context file.")
-		} else if err := utils.ValidateFileExists(filepath.Join(challdir, config.DockerCtx)); err != nil {
-			return fmt.Errorf("file : %s does not exist", config.DockerCtx)
-		}
-
-		if config.XinetdConf == "" {
-			return errors.New("Xinted Context file not provided in docker-type challenge")
-		} else if filepath.IsAbs(config.XinetdConf) {
-			return fmt.Errorf("for challenge type Xinted is a required variable, which should be relative path to Xinted file.")
-		} else if err := utils.ValidateFileExists(filepath.Join(challdir, config.XinetdConf)); err != nil {
-			return fmt.Errorf("file : %s does not exist", config.XinetdConf)
+			return fmt.Errorf("File : %s does not exist", config.DockerCtx)
 		}
 	} else {
 		config.DockerCtx = core.DEFAULT_DOCKER_FILE
@@ -531,10 +517,10 @@ func (config *ChallengeEnv) ValidateRequiredFields(challType string, challdir st
 
 // Metadata related to author of the challenge, this structure includes
 //
-//   - Name - Name of the author of the challenge
-//   - Email - Email of the author
-//   - SSHKey - Public SSH key for the challenge author, to give the access
-//     to the challenge container.
+// * Name - Name of the author of the challenge
+// * Email - Email of the author
+// * SSHKey - Public SSH key for the challenge author, to give the access
+//		to the challenge container.
 //
 // ```toml
 // # Optional fields
