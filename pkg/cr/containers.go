20e66d341afb9d6e661d24bc36887ea14d02a9ac
vibhatsu <maulikbarot2915@gmail.com>
2025-03-17 16:31:45 +0530

    fix(errors): standardize error messages
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- pkg/cr/containers.go
+++ pkg/cr/containers.go
@@ -155,7 +155,7 @@ func CreateContainerFromImage(containerConfig *CreateContainerConfig) (string, e
 	for _, portMapping := range containerConfig.PortMapping {
 		natPort, err := nat.NewPort(containerConfig.TrafficType(), strconv.Itoa(int(portMapping.ContainerPort)))
 		if err != nil {
-			return "", fmt.Errorf("Error while creating new port from port %d", portMapping.ContainerPort)
+			return "", fmt.Errorf("error while creating new port from port %d", portMapping.ContainerPort)
 		}
 
 		portSet[natPort] = struct{}{}
@@ -198,7 +198,7 @@ func CreateContainerFromImage(containerConfig *CreateContainerConfig) (string, e
 
 	createResp, err := cli.ContainerCreate(ctx, config, hostConfig, nil, nil, containerName)
 	if err != nil {
-		log.Error("Error while creating the container with name %s", containerName)
+		log.Errorf("Error while creating the container with name %s", containerName)
 		return "", err
 	}
 
@@ -208,7 +208,7 @@ func CreateContainerFromImage(containerConfig *CreateContainerConfig) (string, e
 	}
 
 	if err := cli.ContainerStart(ctx, containerId, types.ContainerStartOptions{}); err != nil {
-		log.Error("Error while starting the container : %s", err)
+		log.Errorf("Error while starting the container : %s", err)
 		return "", err
 	}
 
