4c3a710b36b39d807599509c81ef58f14e3f2db0
vibhatsu <maulikbarot2915@gmail.com>
2025-03-17 15:54:25 +0530

    feat(ChallengeEnv): DockerCompose field
    Signed-off-by: vibhatsu <maulikbarot2915@gmail.com>

--- core/config/challenge.go
+++ core/config/challenge.go
@@ -269,6 +269,7 @@ type ChallengeEnv struct {
 	Entrypoint       string           `toml:"entrypoint"`
 	DockerCtx        string           `toml:"docker_context"`
 	XinetdConf       string           `toml:"xinetd_conf"`
+	DockerCompose    string           `toml:"docker_compose"`
 	EnvironmentVars  []EnvironmentVar `toml:"var"`
 	Traffic          string           `toml:"traffic"`
 }
