7b3d0c231e3987093c7681fcbf6b509a29c49a0a
sukhman <sukhmansinghsaluja@gmail.com>
2025-08-13 23:15:46 +0530

    Fix database init errors

--- api/main.go
+++ api/main.go
@@ -66,6 +66,7 @@ func RunBeastApiServer(port, defaultauthorpassword string, autoDeploy, healthPro
 	auth.Init(core.ITERATIONS, core.HASH_LENGTH, core.TIMEPERIOD, core.ISSUER, config.Cfg.JWTSecret, []string{core.USER_ROLES["author"]}, []string{core.USER_ROLES["admin"]}, []string{core.USER_ROLES["contestant"]})
 	remoteManager.Init()
 	database.Init()
+	
 	runBeastApiBootsteps(defaultauthorpassword)
 
 	// Initialize Gin router.
