6e682de1ba69ae9e138c29f871a73a770b0a377b
asutosh29 <asutosh29aaz@gmail.com>
2025-10-20 13:26:08 +0530

    update swagger docs for admin routes

--- api/admin.go
+++ api/admin.go
@@ -21,7 +21,7 @@ import (
 // @Success 200 {object} api.ChallengeStatusResp
 // @Failure 400 {object} api.HTTPPlainResp
 // @Failure 500 {object} api.HTTPPlainResp
-// @Router /api/admin/users/:action/:id [post]
+// @Router /api/admin/users/{action}/{id} [post]
 func userActionHandler(c *gin.Context) {
 	action := c.Param("action")
 	userId := c.Param("id")
@@ -40,10 +40,10 @@ func userActionHandler(c *gin.Context) {
 	} else if action == core.USER_STATUS["unban"] {
 		userState = 0
 	} else if action == core.USER_STATUS["hide"] {
-		userState = 2 
+		userState = 2
 	} else if action == core.USER_STATUS["unhide"] {
 		userState = 0
-	} 
+	}
 
 	parsedUserId, err := strconv.Atoi(userId)
 	if err != nil {
@@ -68,7 +68,7 @@ func userActionHandler(c *gin.Context) {
 		})
 		return
 	}
-	if val, _:= database.IsFrozenScoreSet(); !val {
+	if val, _ := database.IsFrozenScoreSet(); !val {
 		leaderboardStale = true
 		graphCacheStale = true
 	}
@@ -76,4 +76,3 @@ func userActionHandler(c *gin.Context) {
 		Message: fmt.Sprintf("Successfully %sned the user with id %s", action, userId),
 	})
 }
-
