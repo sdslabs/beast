18d35b78af606737539ea2258df1ad6947470a23
kunal <kunal22102005@gmail.com>
2026-02-07 16:34:36 +0530

    Fix: fix unnecessary change

--- core/manager/instance.go
+++ core/manager/instance.go
@@ -415,7 +415,7 @@ func deployInstanceContainer(instanceID, challengeName string, imageID string, c
 		}
 	} else {
 		server := cfg.Cfg.AvailableServers[serverDeployed]
-		containerId, err = cr.CreateContainerFromImage(&containerConfig)
+		containerId, err = remoteManager.CreateContainerFromImageRemote(containerConfig, server)
 		if err != nil {
 			return "", "", fmt.Errorf("failed to create container from image: %w", err)
 		}
