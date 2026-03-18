bb0893998adb7a0cfb50a6ae5a4751fa04df1b1c
kunal <kunal22102005@gmail.com>
2026-03-18 22:44:10 +0530

    Fix chown group name

--- core/manager/instance.go
+++ core/manager/instance.go
@@ -405,7 +405,7 @@ func verifySSHRemote(containerId string, server cfg.AvailableServer) error {
 }
 
 func addUserSSHKeyLocal(containerId string, sshKey string) error {
-	sshCmd := fmt.Sprintf("mkdir -p /home/beast/.ssh && echo '%s' >> /home/beast/.ssh/authorized_keys && chmod 700 /home/beast/.ssh && chmod 600 /home/beast/.ssh/authorized_keys && chown -R beast:beast /home/beast/.ssh", sshKey)
+	sshCmd := fmt.Sprintf("mkdir -p /home/beast/.ssh && echo '%s' >> /home/beast/.ssh/authorized_keys && chmod 700 /home/beast/.ssh && chmod 600 /home/beast/.ssh/authorized_keys && chown -R beast:beast-grp /home/beast/.ssh", sshKey)
 
 	result, err := cr.RunCommandInContainer(containerId, []string{
 		"sh", "-c", sshCmd,
@@ -421,7 +421,7 @@ func addUserSSHKeyLocal(containerId string, sshKey string) error {
 }
 
 func addUserSSHKeyRemote(containerId string, server cfg.AvailableServer, sshKey string) error {
-	sshCmd := fmt.Sprintf("mkdir -p /home/beast/.ssh && echo '%s' >> /home/beast/.ssh/authorized_keys && chmod 700 /home/beast/.ssh && chmod 600 /home/beast/.ssh/authorized_keys && chown -R beast:beast /home/beast/.ssh", sshKey)
+	sshCmd := fmt.Sprintf("mkdir -p /home/beast/.ssh && echo '%s' >> /home/beast/.ssh/authorized_keys && chmod 700 /home/beast/.ssh && chmod 600 /home/beast/.ssh/authorized_keys && chown -R beast:beast-grp /home/beast/.ssh", sshKey)
 
 	result, err := remoteManager.RunCommandInContainerOnServer(server, containerId, sshCmd)
 	if err != nil {
