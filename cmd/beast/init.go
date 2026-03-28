4cb8eea477b7c60e9bfc995b891ea744df0fbaf2
kunal <kunal22102005@gmail.com>
2026-03-28 19:16:14 +0530

    Add acl save and fix sprintf typos

--- cmd/beast/init.go
+++ cmd/beast/init.go
@@ -121,6 +121,12 @@ func createBeastRedisUser(cache *redis.Client, configuration *config.RedisConfig
 	}
 
 	log.Infoln(fmt.Sprintf("Initialised redis user %s", configuration.User))
+
+	err = cache.Do(ctx, "acl", "save").Err()
+	if err != nil {
+		return fmt.Errorf("error while trying to save the acl file: %s", err.Error())
+	}
+
 	return nil
 }
 
@@ -137,14 +143,14 @@ func initCache() error {
 		})
 	} else {
 		cache = redis.NewClient(&redis.Options{
-			Addr:     fmt.Sprint("%s:%s", redisConfig.Host, redisConfig.Port),
+			Addr:     fmt.Sprintf("%s:%s", redisConfig.Host, redisConfig.Port),
 			Username: core.REDIS_DEFAULT_USER,
 		})
 	}
 
 	_, err := cache.Ping(context.Background()).Result()
 	if err != nil {
-		return fmt.Errorf("failed to connected to redis")
+		return fmt.Errorf("failed to connected to redis: %s", err.Error())
 	}
 
 	defer cache.Close()
