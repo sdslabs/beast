f2a097801c9a2c5ab4ec3b579e5d90431b4f476c
Garvit Sharma <sharmagarvit614@gmail.com>
2026-04-10 00:31:51 +0000

    refactor: optimize slice allocation

--- api/info.go
+++ api/info.go
@@ -355,7 +355,7 @@ func challengesMetadataHandler(c *gin.Context) {
 			}
 		}
 
-		availableChallenges := make([]ChallengeMetadata, len(challenges))
+		availableChallenges := make([]ChallengeMetadata, 0, len(challenges))
 
 		authHeader := c.GetHeader("Authorization")
 		username, err := coreUtils.GetUser(authHeader)
@@ -374,7 +374,7 @@ func challengesMetadataHandler(c *gin.Context) {
 			return
 		}
 
-		for index, challenge := range challenges {
+		for _, challenge := range challenges {
 			if challenge.Status == "Undeployed" && user.Role == core.USER_ROLES["contestant"] {
 				continue
 			}
@@ -388,11 +388,11 @@ func challengesMetadataHandler(c *gin.Context) {
 			}
 			challengeTags := make([]string, len(challenge.Tags))
 
-			for index, tags := range challenge.Tags {
-				challengeTags[index] = tags.TagName
+			for i, tags := range challenge.Tags {
+				challengeTags[i] = tags.TagName
 			}
 
-			availableChallenges[index] = ChallengeMetadata{
+			availableChallenges = append(availableChallenges, ChallengeMetadata{
 				Name:               challenge.Name,
 				ChallId:            challenge.ID,
 				Tags:               challengeTags,
@@ -405,7 +405,7 @@ func challengesMetadataHandler(c *gin.Context) {
 				DeployedStatus:     challenge.Status,
 				Instanced:          challenge.Instanced,
 				InstanceExpiration: challenge.InstanceExpiration,
-			}
+			})
 		}
 
 		c.JSON(http.StatusOK, availableChallenges)
