8a7d7c201d3af51b8d529fbabb20dc4435135958
kunal <kunal22102005@gmail.com>
2026-04-16 03:31:35 +0530

    Remove ssh-agent check

--- core/manager/instance.go
+++ core/manager/instance.go
@@ -1,7 +1,6 @@
 package manager
 
 import (
-	"bytes"
 	"fmt"
 	"os/exec"
 	"path/filepath"
@@ -427,18 +426,7 @@ func verifySSHLocal(containerId string) error {
 		return err
 	}
 
-	sshAgentCheckCommand := fmt.Sprintf("[ -S \"$SSH_AUTH_SOCK\" ]")
-	result, err := cr.RunCommandInContainer(containerId, []string{
-		"sh", "-c", sshAgentCheckCommand,
-	})
-
-	if err != nil {
-		return err
-	}
-	if result.ExitCode != 0 {
-		return fmt.Errorf("ssh agent check failed, ssh-agent not running")
-	}
-
+	var result cr.ExecResult
 	sshServiceStartCommand := fmt.Sprintf("service ssh start")
 	result, err = cr.RunCommandInContainer(containerId, []string{
 		"sh", "-c", sshServiceStartCommand,
@@ -461,16 +449,7 @@ func verifySSHRemote(containerId string, server cfg.AvailableServer) error {
 		return err
 	}
 
-	sshAgentCheckCommand := fmt.Sprintf("[ -S \"$SSH_AUTH_SOCK\" ]")
-	result, err := remoteManager.RunCommandInContainerOnServer(server, containerId, sshAgentCheckCommand)
-
-	if err != nil {
-		return err
-	}
-	if result.ExitCode != 0 {
-		return fmt.Errorf("ssh agent check failed, ssh-agent not running")
-	}
-
+	var result cr.ExecResult
 	sshServiceStartCommand := fmt.Sprintf("service ssh start")
 	result, err = cr.RunCommandInContainer(containerId, []string{
 		"sh", "-c", sshServiceStartCommand,
